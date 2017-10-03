06c1786a8f796092b2e0cdf4cf388f1a4149bd99
Rbatts <32482377+Rbatts@users.noreply.github.com>
2017-10-03 16:13:11 +0100

    adding strings

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -12,17 +12,29 @@ namespace ConsoleApp1
         {
             for (var i = 1; i <= 100; i = i + 1)
             {
-                if (i % 15 == 0)
+                if  (i % 11 == 0)
+                {
+                    Console.WriteLine("Bong");
+                }
+                else if (i % 21 == 0)
+                {
+                    Console.WriteLine("FizzBang");
+                }
+                else if (i % 7 == 0) 
+                {
+                    Console.WriteLine("Bang");
+                }
+                else if (i % 15 == 0)
                 {
                     Console.WriteLine("FizzBuzz");
-                }
-                else if (i % 5 == 0)
-                {
-                    Console.WriteLine("Buzz");
                 }
                 else if (i % 3 == 0)
                 {
                     Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
                 }
                 else
                 {
