5e539e3579c379827b05c0b7ef8f539405bcde4e
Rbatts <32482377+Rbatts@users.noreply.github.com>
2017-10-03 14:39:18 +0100

    first section completed

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 1; i <= 100; i = i + 1)
+            {
+                if (i % 15 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
